eb3ca3225328cdde75d158b77fdc47f9428681e5
Piotr Komborski <piotr.komborski@gmail.com>
2019-06-09 11:51:26 +0100

    Brew formula update for p version v0.0.16

--- Formula/p.rb
+++ Formula/p.rb
@@ -2,9 +2,9 @@
 class P < Formula
   desc "P is a cli for Potantially."
   homepage ""
-  url "http://potentially-p-releases.s3.eu-west-1.amazonaws.com/v0.0.15/p_0.0.15_darwin_amd64.zip"
-  version "0.0.15"
-  sha256 "78cb76c629982a3984f5150f835d629b459160e6794f3176bf129c87a3560ddc"
+  url "http://potentially-p-releases.s3.eu-west-1.amazonaws.com/v0.0.16/p_0.0.16_darwin_amd64.zip"
+  version "0.0.16"
+  sha256 "77783dfdab6548ab830be1e9cb51514bebc1e080dcdc556db6126a9149aa766b"
 
   def install
     bin.install "p"
